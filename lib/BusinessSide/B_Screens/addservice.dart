0faa9ec1815346cc1ce49f352708dc542c7e7ee4
arshad-lashari <arshadlashari02772@gmail.com>
2024-09-01 00:53:25 +0500

    MelaApp

--- lib/BusinessSide/B_Screens/addservice.dart
+++ lib/BusinessSide/B_Screens/addservice.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mela/constant/apptext.dart';
 import 'package:mela/constant/colorspath.dart';
 import 'package:mela/constant/imagespath.dart';
@@ -11,23 +14,73 @@ class AddServiceScreen extends StatefulWidget {
   State<AddServiceScreen> createState() => _AddServiceScreenState();
 }
 
-class _AddServiceScreenState extends State<AddServiceScreen>
-    with SingleTickerProviderStateMixin {
+class _AddServiceScreenState extends State<AddServiceScreen> {
   AppColors appColors = AppColors();
   AppText appText = AppText();
   AppImagesPath appImagesPath = AppImagesPath();
-  late AnimationController _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
+  // Define a list of services and a selected service
+  final List<String> _services = ['Cleaner', 'Home Cleaner', 'Car Cleaner'];
+  String? _selectedService;
+
+  // To store the picked image
+  File? _image;
+
+  Future<void> _pickImage(ImageSource source) async {
+    final ImagePicker _picker = ImagePicker();
+    try {
+      final XFile? pickedImage = await _picker.pickImage(source: source);
+      if (pickedImage != null) {
+        setState(() {
+          _image = File(pickedImage.path);
+        });
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void _showImageSourceOptions() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.white,
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton.icon(
+              icon: const Icon(
+                Icons.image,
+                color: Colors.black54,
+              ),
+              label: const Text(
+                'Gallery',
+                style: TextStyle(color: Colors.black54),
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                _pickImage(ImageSource.gallery);
+              },
+            ),
+            TextButton.icon(
+              icon: const Icon(
+                Icons.camera_alt,
+                color: Colors.black54,
+              ),
+              label: const Text(
+                'Camera',
+                style: TextStyle(color: Colors.black54),
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                _pickImage(ImageSource.camera);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -50,32 +103,107 @@ class _AddServiceScreenState extends State<AddServiceScreen>
                 backgroundColor: Colors.white,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5))),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pop(context);
+            },
             icon: const Icon(Icons.arrow_back)),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              height: 158,
-              width: 158,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: Column(
-                children: [
-                  Icon(
-                    Icons.image_outlined,
-                    color: Colors.black,
-                  )
-                ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: _showImageSourceOptions,
+              child: Container(
+                height: 158,
+                width: 158,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: _image == null
+                    ? const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            CupertinoIcons.photo,
+                            color: Colors.black54,
+                            size: 24,
+                          ),
+                          Text(
+                            'Add Picture',
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      )
+                    : ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.file(
+                          _image!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
               ),
             ),
-          )
-        ],
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'Service',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Ubuntu',
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.darkblue),
+              ),
+            ),
+            // Dropdown for service selection
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                value: _selectedService,
+                hint: const Text('Select Service'),
+                items: _services.map((String service) {
+                  return DropdownMenuItem<String>(
+                    value: service,
+                    child: Text(service),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _selectedService = newValue;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
